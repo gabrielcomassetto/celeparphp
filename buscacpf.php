65b96eab417a0298102936245711896780912c7b
gabrielcomassetto <77698431+gabrielcomassetto@users.noreply.github.com>
2022-06-27 09:37:49 -0300

    Update buscacpf.php

--- buscacpf.php
+++ buscacpf.php
@@ -19,6 +19,7 @@ $consulta = mysqli_query($conexao, $query);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="tabela.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
 </head>
@@ -38,6 +39,15 @@ $consulta = mysqli_query($conexao, $query);
     
 <?php
 echo "<table class='table table-striped'>";
+    echo"<tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>CPF</th>
+        <th>Email</th>
+        <th>Cidade</th>
+        <th>Estado</th>
+        <th></th></tr>";
+
     while($row = mysqli_fetch_array($consulta)){
         $id = $row[0];
         $nome = $row[1];
@@ -47,20 +57,13 @@ echo "<table class='table table-striped'>";
         $estado = $row[6];
 
         echo"<tr>
-        <th>ID</th>
-        <th>Nome</th>
-        <th>CPF</th>
-        <th>Email</th>
-        <th>Cidade</th>
-        <th>Estado</th></tr>";
-
-        echo"<tr>
         <td>$id</td>
         <td>$nome</td>
         <td>$cpf</td>
         <td>$email </td>
         <td>$cidade</td>
-        <td>$estado</td></tr>";
+        <td>$estado</td>
+        <td><a href='editarusuario.php'><i class='fa fa-edit' href='editarusuario.php'></i></a></td></tr>";
         
 }
 echo "</table>";
